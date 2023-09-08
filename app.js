cd1917311ebb77fe9cd484df6f6646e9ec975bce
1day1commit <heytaxitaxi@gmail.com>
2023-09-08 14:02:11 +0800

    230908

--- app.js
+++ app.js
@@ -36,3 +36,18 @@ function typeText() {
 
 // Start typing effect
 setTimeout(typeText, 400);
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+          targetElement.scrollIntoView({
+              behavior: 'smooth'
+          });
+      }
+  });
+});
