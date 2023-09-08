0335fdad4b9cdddc3844a3ae39b13d55bbb432a1
1day1commit <heytaxitaxi@gmail.com>
2023-09-08 13:46:46 +0800

    123

--- app.js
+++ app.js
@@ -8,3 +8,31 @@
 // cards.forEach((card) => {
 //   observer.observe(card);
 // });
+
+const textElement = document.getElementById("intro-text");
+const texts = ["writes codes.", "thinks out of the box.", "solves problems."];
+let currentTextIndex = 0;
+let currentCharIndex = 0;
+let isDeleting = false;
+
+function typeText() {
+    const currentText = texts[currentTextIndex];
+    if (!isDeleting && currentCharIndex <= currentText.length) {
+        textElement.innerHTML = currentText.substring(0, currentCharIndex);
+        currentCharIndex++;
+        setTimeout(typeText, 50); // Adjust typing speed here
+    } else if (isDeleting && currentCharIndex >= 0) {
+        textElement.innerHTML = currentText.substring(0, currentCharIndex);
+        currentCharIndex--;
+        setTimeout(typeText, 50); // Adjust erasing speed here
+    } else {
+        isDeleting = !isDeleting;
+        if (!isDeleting) {
+            currentTextIndex = (currentTextIndex + 1) % texts.length;
+        }
+        setTimeout(typeText, 600); // Time before switching to the next text
+    }
+}
+
+// Start typing effect
+setTimeout(typeText, 400);
